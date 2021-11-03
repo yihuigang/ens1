81694a70494c593fbcf4ea03c8548b0316b9c9fa
yhg <huigang.yi@logwirecloud.com>
2021-11-03 19:05:32 +0800

    ens1

---
+++ README.md
@@ -0,0 +1,39 @@
+# 说明
+
+## 代码段
+
+### 支持代码段：
+
+query
+
+find
+
+findAll
+
+sqlquery
+
+insert
+
+insertbatch
+
+update(更新指定字段)
+
+updatemap(更新行)
+
+updatebatch(批量更新)
+
+delete
+
+foreach
+
+tasks,计划任务
+
+redirect(指定跳转)
+
+log,rizhi(日志)
+
+## 发布命令
+
+1.cmd切换到项目目录：cd ens1
+
+2.cmd打包发布：vsce package
---
+++ src/extension.js
@@ -0,0 +1,21 @@
+const vscode = require('vscode');
+
+/**
+ * 插件被激活时触发，所有代码总入口
+ * @param {*} context 插件上下文
+ */
+exports.activate = function (context) {
+    // console.log('恭喜，您的扩展“vscode-plugin-demo”已被激活！');
+    // 注册命令
+    context.subscriptions.push(vscode.commands.registerCommand('extension.openUrl', function (uri) {
+        vscode.window.showInformationMessage('openUrl' + uri.path);
+        window.open("https://www.logwirecloud.com/cn/");
+    }));
+};
+
+/**
+ * 插件被释放时触发
+ */
+exports.deactivate = function () {
+    // console.log('您的扩展“vscode-plugin-demo”已被释放！');
+};
