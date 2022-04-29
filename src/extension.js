05544625097a91577eaebbccceade6bac6e5b006
yhg <huigang.yi@logwirecloud.com>
2022-04-29 16:27:03 +0800

    列标签调整

--- src/extension.js
+++ src/extension.js
@@ -8,9 +8,12 @@ exports.activate = function (context) {
     // console.log('恭喜，您的扩展“vscode-plugin-demo”已被激活！');
     // 注册命令
     context.subscriptions.push(vscode.commands.registerCommand('extension.openUrl', function (uri) {
-        vscode.window.showInformationMessage('openUrl' + uri.path);
-        window.open("https://www.logwirecloud.com/cn/");
-        console.log("openUrl" + uri.path);
+        // vscode.window.showInformationMessage('openUrl' + uri.path);
+        // window.open("https://www.logwirecloud.com/cn/");
+        var msg = workspaceFolders[0] + workspaceFile[0];
+        Clipboard.writeText(msg);
+        console.log(msg);
+        // workspaceFolders[0]
     }));
 };
 
